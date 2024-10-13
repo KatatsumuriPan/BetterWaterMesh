825b9c8df87d2c9dd1eb13bf744be38fc22c1c4b
KatatsumuriPan <katatsumuri.pan@gmail.com>
2024-10-13 11:10:41 +0900

    Initialize modding

---
+++ src/main/java/kpan/b_water_mesh/proxy/CommonProxy.java
@@ -0,0 +1,8 @@
+package kpan.b_water_mesh.proxy;
+
+@SuppressWarnings("unused")
+public class CommonProxy {
+	public void registerOnlyClient() { }
+
+	public boolean hasClientSide() { return false; }
+}
